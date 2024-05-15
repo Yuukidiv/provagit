cbc353f117c6ecc500b657250d8cd7e4fd6ea569
Yuuki <67429515+Yuukidiv@users.noreply.github.com>
2024-05-15 19:03:33 +0200

    Update Giocatore.java

--- giocatore/Giocatore.java
+++ giocatore/Giocatore.java
@@ -1,7 +1,7 @@
 package it.uniroma3.diadia.giocatore;
 
 public class Giocatore {
-	static final private int CFU_INIZIALI = 20;
+	static final private int CFU_INIZIALI = 100;
 
 	private int cfu;
 	private Borsa borsa;
